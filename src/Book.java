9018a29eb7a8d202d9e3deb46ff9fd4409beb76e
Amani-it1 <Amani.ab7@hotmail.com>
2026-07-13 00:23:44 +0300

    Add Book class with title, author, and year fields
    Add Book class

---
+++ src/Book.java
@@ -0,0 +1,21 @@
+
+public class Book {
+	
+	String title;
+	String author;
+	int year;
+	
+	
+	public Book(String title, String author , int year) {
+	this.title = title;
+	this.author = author;
+	this.year = year ;
+
+}
+public String toString() {
+	return "Title:"+title +
+			",Author:"+ author +
+			",Year:"+year ;
+}
+			
+}

e16d8a9ed59aeb56c80a04d2029ccc08361ee728
Wasan <wasan.binhadba@gmail.com>
2026-07-13 16:12:05 +0300

    Added LinkedList implemenation

---
+++ src/BookInquirySystemLinkedList.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class BookInquirySystemLinkedList {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		LinkedList<Book> books = new LinkedList<>();
+		books.add(new Book("Java Programming", "James Gosling", 2020));
+		books.add(new Book("Data Structures", "Mark Allen", 2019));
+		books.add(new Book("Database System", "Thomas Connolly", 2021));
+		books.add(new Book("Computer Networks", "Andrew Tanenbaum", 2018));
+
+		int choice;
+
+		do {
+
+		    System.out.println("\n===== Book Inquiry System =====");
+		    System.out.println("1. Display Books");
+		    System.out.println("2. Add Book");
+		    System.out.println("3. Search Book");
+		    System.out.println("4. Delete Book");
+		    System.out.println("5. Exit");
+
+		    System.out.print("Enter your choice: ");
+		    choice = input.nextInt();
+		    input.nextLine();
